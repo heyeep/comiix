2d3166072a94f3a19102cb49a86b97a3da72c608
Hiep Nguyen <hiepism@gmail.com>
2021-01-12 13:57:15 -0800

    Add an api to test seed functionality

--- server/views.py
+++ server/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .tasks.seeder import seed_db
 
 # Create your views here.
+def index(req):
+    return HttpResponse('Sup')
+def seed(request):
+    if request.POST:
+        task_type = request.POST.get('type')
+        task = seed_db()
+        return JsonResponse({'task_id': task.id, 'object': task}, status=202)
