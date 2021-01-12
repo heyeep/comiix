5e248bb783fb5acdd052cfa2e12cde53f0d420ca
Hiep Nguyen <hiepism@gmail.com>
2021-01-12 10:36:02 -0800

    Add constraints for checking if Author exists

--- server/models/author.py
+++ server/models/author.py
@@ -5,5 +5,15 @@ class Author(models.Model):
     last_name = models.CharField(default='None', max_length=25)
     middle_name = models.CharField(default='None', max_length=25)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['first_name', 'middle_name', 'last_name'],
+                name='author_full_name'),
+            models.CheckConstraint(
+                check=models.Q(first_name is not None),
+                name='author_check_if_first_name_exists')
+        ]
+
     def __str__(self):
         return self.full_name()
